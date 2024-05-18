1594a966df35d68b55f3a6e71918dd66c43afe5d
Meng Xipeng <mengxipeng@gmail.com>
2024-05-18 17:03:15 +0800

    remove index.js in my-library

--- my-library/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-function helloWorld() {
-    return 'Hello, world!';
-}
-exports.helloWorld = helloWorld;
